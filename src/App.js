be3cac3308f788db701b71ac188fe16c170d54cf
Jeremiah Marrujo <marrujojeremiah@gmail.com>
2024-07-29 16:07:14 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import projects from './Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div ClassName="App">
+      <header ClassName="App-header">
+        <h1>Welcome to my Portfolio</h1>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
       </header>
+      <section id="home">
+        <h2>Home</h2>
+        <p>Welcome to my personal portfolio website</p>
+      </section>
+      <section id="projects">
+        <h2>Projects</h2>
+      </section>
+      <section id="about">
+        <h2>About</h2>
+        <p>Information about me.</p>
+      </section>
+      <section id="contact">
+        <h2>Contact</h2>
+        <p>Get in touch with me.</p>
+      </section>
     </div>
   );
 }
